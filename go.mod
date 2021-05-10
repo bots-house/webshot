096ba10bea700bfce3a32bf1027164e883d3ca2d
Sasha Savchuk <sasha@bots.house>
2021-05-11 01:38:22 +0300

    feat(main): support cli and env

--- go.mod
+++ go.mod
@@ -5,8 +5,9 @@ go 1.16
 require (
 	github.com/chromedp/cdproto v0.0.0-20210508221054-d7cfa85db7d1
 	github.com/chromedp/chromedp v0.7.1
+	github.com/jessevdk/go-flags v1.5.0 // indirect
 	github.com/rs/zerolog v1.21.0
 	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
-	golang.org/x/sys v0.0.0-20210503173754-0981d6026fa6 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 )
--- main.go
+++ main.go
@@ -1,24 +1,125 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/bots-house/webshot/internal/api"
 	"github.com/bots-house/webshot/internal/renderer"
+	"github.com/jessevdk/go-flags"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+)
+
+type Config struct {
+	HTTP struct {
+		Addr string `long:"addr" description:"http addr to listen" env:"ADDR" default:":8000"`
+	} `group:"HTTP" namespace:"http" env-namespace:"HTTP"`
+}
+
+func loadConfig() Config {
+	config := Config{}
+
+	parser := flags.NewParser(&config, flags.Default)
+	if _, err := parser.Parse(); err != nil {
+		switch flagsErr := err.(type) {
+		case flags.ErrorType:
+			if flagsErr == flags.ErrHelp {
+				os.Exit(0)
+			}
+			fmt.Print(err)
+			os.Exit(1)
+		default:
+			fmt.Print(err)
+			os.Exit(1)
+		}
+	}
+
+	return config
+}
+
+var (
+	buildVersion = "unknown"
+	buildRef     = "unknown"
+	buildTime    = "unknown"
 )
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	addr := "0.0.0.0:8000"
+	config := loadConfig()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ctx, cancel = signal.NotifyContext(ctx, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	log.Ctx(ctx).Info().
+		Dict("build", zerolog.Dict().
+			Str("version", buildVersion).
+			Str("ref", buildRef).
+			Str("time", buildTime),
+		).
+		Msg("start webshot")
+
+	if err := run(ctx, config); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		defer os.Exit(2)
+	}
+}
+
+func run(ctx context.Context, config Config) error {
+	log.Info().Str("addr", config.HTTP.Addr).Msg("listen http...")
 
 	rndr := &renderer.Chrome{Debug: false}
 
 	api := api.New(api.Deps{Renderer: rndr})
 
-	log.Info().Str("addr", "http://"+addr).Msg("listen http")
-	http.ListenAndServe("0.0.0.0:8000", api)
+	server := newServer(
+		config.HTTP.Addr,
+		api,
+	)
+
+	go func() {
+		<-ctx.Done()
+
+		log.Warn().Msg("shutdown signal received")
+
+		shutdownCtx, cancel := context.WithTimeout(
+			context.Background(),
+			time.Second*10,
+		)
+
+		defer cancel()
+
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			log.Warn().Err(err).Msg("shutdown error")
+		}
+	}()
+
+	if err := server.ListenAndServe(); err == http.ErrServerClosed {
+		return nil
+	} else {
+		return err
+	}
+}
+
+func newServer(addr string, handler http.Handler) *http.Server {
+	baseCtx := context.Background()
+	baseCtx = log.Logger.WithContext(baseCtx)
+
+	return &http.Server{
+		Addr:    addr,
+		Handler: handler,
+		BaseContext: func(_ net.Listener) context.Context {
+			return baseCtx
+		},
+	}
 }
